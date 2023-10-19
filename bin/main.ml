b60daa77d296b750fa6b3f33b669929d21518e76
Gerardo <gm547@cornell.edu>
2023-10-18 22:58:13 -0400

    Changed the title screen art

--- bin/main.ml
+++ bin/main.ml
@@ -1,27 +1,29 @@
 let () = print_endline "Starting MTD!"
-
+open Raylib
 let setup () =
-  Raylib.init_window 700 450 "raylib [core] example - basic window";
-  Raylib.set_target_fps 60
+  Raylib.init_window 900 650 "raylib [core] example - basic window";
+  Raylib.set_target_fps 60;
+  let loading_screen = Raylib.load_image "MTDCoverArt.png" in 
+  let texture = Raylib.load_texture_from_image loading_screen in
+  unload_image loading_screen;
+  (texture)
 
-let rec loop () =
+let rec loop (texture) =
   if Raylib.window_should_close () then Raylib.close_window ()
   else
     let open Raylib in
-    let loading_screen = Raylib.load_image "loadscreen.png" in
-    let texture = Raylib.load_texture_from_image loading_screen in
-    ignore (texture);
+   
     begin_drawing ();
     clear_background Color.raywhite;
     (draw_texture_ex texture       
     (Vector2.create 0. 0.0)  (* Position *)
     0.0                          (* Rotation (in radians) *)
-    (0.42)                (* Scale *)
+    (0.70)                (* Scale *)
     Color.white);
-    draw_text "McGraw Tower" 40 200 50
+    draw_text "McGraw Tower" 430 175 60
       Color.red;
-    draw_text "Defense" 40 250 50 Color.red;
+    draw_text "Defense!" 490 250 60 Color.red;
     end_drawing ();
-    loop ()
+    loop (texture)
 
 let () = setup () |> loop
