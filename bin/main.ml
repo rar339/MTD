8dbc046f7a41707af2fc41b421b2883e4af46428
Rolando Rodríguez <rar339@cornell.edu>
2023-10-20 09:45:21 -0400

    More balloons and improved wrapping

--- bin/main.ml
+++ bin/main.ml
@@ -21,10 +21,26 @@ let setup () =
 
   (texture, red_bal_texture)
 
-let pos = ref 550.
+let pos1 = ref 550.
+
+let pos2 = ref 650.
+
+let pos3 = ref 800. 
+
+let pos4 = ref 775.
+
+let pos5 = ref 900.
+
+let pos6 = ref 1100.
+
+let pos7 = ref 1120.
+
+let pos8 = ref 1300. 
+
+let pos9 = ref 1400.
 
 let update_balloon_position(pos) = 
-  if !pos < 2. then pos:= 650. else pos:= !pos -. 0.09; pos 
+  if !pos < ~-.5. then pos:= 650. else pos:= !pos -. 0.2; pos 
 
 let rec loop (tuple) =
   if Raylib.window_should_close () then Raylib.close_window ()
@@ -61,29 +77,49 @@ let rec loop (tuple) =
 
     (***** BALLOONS *****)
     (draw_texture_ex red_bal_texture       
-    (Vector2.create (500.) !(update_balloon_position(pos)))  (* Position *)
-    0.0                          (* Rotation (in radians) *)
-    (0.15)                (* Scale *)
+    (Vector2.create (500.) !(update_balloon_position(pos1))) 
+    0.0                       
+    (0.15)              
     Color.white);
     (draw_texture_ex red_bal_texture       
-    (Vector2.create (600.) (50. +. !(update_balloon_position(pos))))  (* Position *)
-    0.0                          (* Rotation (in radians) *)
-    (0.15)                (* Scale *)
+    (Vector2.create (823.) (!(update_balloon_position(pos2)))) 
+    0.0                         
+    (0.15)                
     Color.white);
     (draw_texture_ex red_bal_texture       
-    (Vector2.create (300.) (300. +. !(update_balloon_position(pos))))  (* Position *)
-    0.0                          (* Rotation (in radians) *)
-    (0.15)                (* Scale *)
+    (Vector2.create (341.) (!(update_balloon_position(pos3))))  
+    0.0                       
+    (0.15)              
     Color.white);
     (draw_texture_ex red_bal_texture       
-    (Vector2.create (700.) (100. +. !(update_balloon_position(pos))))  (* Position *)
-    0.0                          (* Rotation (in radians) *)
-    (0.15)                (* Scale *)
+    (Vector2.create (763.) (!(update_balloon_position(pos4))))  
+    0.0                         
+    (0.15)                
     Color.white);
     (draw_texture_ex red_bal_texture       
-    (Vector2.create (740.) (200. +. !(update_balloon_position(pos))))  (* Position *)
-    0.0                          (* Rotation (in radians) *)
-    (0.15)                (* Scale *)
+    (Vector2.create (240.) (!(update_balloon_position(pos5)))) 
+    0.0                         
+    (0.15)           
+    Color.white);
+    (draw_texture_ex red_bal_texture       
+    (Vector2.create (521.) (!(update_balloon_position(pos6)))) 
+    0.0                         
+    (0.15)              
+    Color.white);
+    (draw_texture_ex red_bal_texture       
+    (Vector2.create (780.) (!(update_balloon_position(pos7))))  
+    0.0                         
+    (0.15)                
+    Color.white);
+    (draw_texture_ex red_bal_texture       
+    (Vector2.create (620.) (!(update_balloon_position(pos8))))  
+    0.0                    
+    (0.15)                
+    Color.white);
+    (draw_texture_ex red_bal_texture       
+    (Vector2.create (250.) (!(update_balloon_position(pos9))))  
+    0.0                    
+    (0.15)                
     Color.white);
 
     end_drawing ();
