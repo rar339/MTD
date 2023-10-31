b6ecd55f3e38a164cdff0fa9acf1dfa4d7d12f19
Gerardo <gm547@cornell.edu>
2023-10-31 08:53:48 -0400

    Added a reference grid for map

--- lib/game.ml
+++ lib/game.ml
@@ -5,21 +5,31 @@ let count = ref 0
 let background_width : int ref = ref 0
 let background_height : int ref = ref 0
 let background : Texture2D.t option ref = ref None
+let grid_size = 10
+let grid_spacing = 20
+let grid_color = Color.create 255 255 255 255
+let step_value = 10 (* Set your desired step value *)
+
+
 
 let setup () =
   let game_image : Image.t = Raylib.load_image "mtd_map.png" in
   background := Some (load_texture_from_image game_image);
   background_width := Image.width game_image;
   background_height := Image.height game_image;
+
   ()
 
+
+
 let update_game () = ()
+
 
 let draw_game () =
   let open Raylib in
   begin_drawing ();
   clear_background Color.white;
-
+  (*Draw the background*)
   draw_texture_pro (Option.get !background)
     (Rectangle.create 0. 0. 2388. 1668.)
     (Rectangle.create 0. 0.
@@ -27,6 +37,15 @@ let draw_game () =
        (float_of_int !screen_height))
     (Vector2.create 0. 0.) 0.
     (Color.create 255 255 255 255);
+  (*Draw the reference grid *)
+  let x = ref 0 in
+  let y = ref 0 in
+  while !x < !screen_width do
+    draw_line !x 0 !x !screen_height Color.black;
+    x := !x + !screen_width / 40;
+    draw_line 0 !y !screen_width !y Color.black;
+    y := !y + !screen_height / 28;
+  done;
 
   end_drawing ()
 
