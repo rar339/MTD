2c07acf5dbc5d502f1ef0191a89a5a0172fba998
Rolando Rodríguez <rar339@cornell.edu>
2023-11-01 16:41:08 -0400

    More balloons and waves.ml

--- lib/balloons.ml
+++ lib/balloons.ml
@@ -1,35 +1,40 @@
 type colors =
-    | Red of int
-    | Blue of int
-    | White of int
-    | Black of int
-    | Brown of int
-    | Yellow of int
-    | Lead of int
+  | None
+  | Red of int
+  | Blue of int
+  | White of int
+  | Black of int
+  | Brown of int
+  | Yellow of int
+  | Lead of int
 
-  type balloon = {
-    mutable color : colors;
-    mutable velocity : Raylib.Vector2.t;
-    mutable position : Raylib.Vector2.t;
-    mutable is_lead : bool;
-    mutable img : string;
+type balloon = {
+  mutable color : colors;
+  mutable velocity : Raylib.Vector2.t;
+  mutable position : Raylib.Vector2.t;
+  mutable next_down : colors;
+  mutable is_lead : bool;
+  mutable img : string;
+  order : int;
+}
+
+let make_redb i = {
+  color = Red 1;
+  velocity = Raylib.Vector2.create 1.0 1.0;
+  position = Raylib.Vector2.create 1.0 1.0;
+  next_down = None;
+  is_lead = false;
+  img = "red.png";
+  order = i;
   }
 
-  let make_redb () = {
-      color = Red 1;
-      velocity = Raylib.Vector2.create 1.0 1.0;
-      position = Raylib.Vector2.create 1.0 1.0;
-      is_lead = false;
-      img = "red.png";
-  }
-
-  let make_blueb() = {
-    color = Blue 1;
+let make_blueb i = {
+    color = Blue 2;
     velocity = Raylib.Vector2.create 1.5 1.5;
     position = Raylib.Vector2.create 1.0 1.0;
+    next_down = Red 1;
     is_lead = false;
     img = "blue.png";
-  }
-
-
+    order = i;
+}
 
