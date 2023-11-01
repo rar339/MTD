313612f096614110d043e4f3d6b64056f90e48ec
Gerardo <gm547@cornell.edu>
2023-10-31 23:43:05 -0400

    Added all the necessary rectangles that indicate invalid positions to place defenses.

--- lib/game.ml
+++ lib/game.ml
@@ -65,9 +65,64 @@ let setup () =
 
   path_rectangles :=
     create_rectangle 0.
-      (2. *. !screen_height /. 28.)
-      (23. *. !screen_width /. 40.)
-      (2. *. !screen_height /. 28.)
+      (2. *. floor (!screen_height /. 28.))
+      (23. *. floor (!screen_width /. 40.))
+      (2. *. floor (!screen_height /. 28.))
+    :: create_rectangle
+         (21. *. floor (!screen_width /. 40.))
+         (4. *. floor (!screen_height /. 28.))
+         (2. *. floor (!screen_width /. 40.))
+         (5. *. floor (!screen_height /. 28.))
+    :: create_rectangle
+         (3. *. floor (!screen_width /. 40.))
+         (7. *. floor (!screen_height /. 28.))
+         (18. *. floor (!screen_width /. 40.))
+         (2. *. floor (!screen_height /. 28.))
+    :: create_rectangle
+         (3. *. floor (!screen_width /. 40.))
+         (9. *. floor (!screen_height /. 28.))
+         (2. *. floor (!screen_width /. 40.))
+         (19. *. floor (!screen_height /. 29.))
+    :: create_rectangle
+         (5. *. floor (!screen_width /. 40.))
+         (26. *. floor (!screen_height /. 29.))
+         (19. *. floor (!screen_width /. 40.))
+         (3. *. floor (!screen_height /. 38.))
+    :: create_rectangle
+         (24. *. floor (!screen_width /. 40.))
+         (18. *. floor (!screen_height /. 25.))
+         (2. *. floor (!screen_width /. 40.))
+         (8. *. floor (!screen_height /. 31.))
+    :: create_rectangle
+         (8. *. floor (!screen_width /. 40.))
+         (12. *. floor (!screen_height /. 28.))
+         (2. *. floor (!screen_width /. 40.))
+         (11. *. floor (!screen_height /. 30.))
+    :: create_rectangle
+         (10. *. floor (!screen_width /. 40.))
+         (24. *. floor (!screen_height /. 33.))
+         (14. *. floor (!screen_width /. 40.))
+         (3. *. floor (!screen_height /. 37.))
+    :: create_rectangle
+         (10. *. floor (!screen_width /. 40.))
+         (12. *. floor (!screen_height /. 28.))
+         (3. *. floor (!screen_width /. 40.))
+         (3. *. floor (!screen_height /. 36.))
+    :: create_rectangle
+         (13. *. floor (!screen_width /. 40.))
+         (12. *. floor (!screen_height /. 28.))
+         (2. *. floor (!screen_width /. 40.))
+         (5. *. floor (!screen_height /. 27.))
+    :: create_rectangle
+         (15. *. floor (!screen_width /. 40.))
+         (15. *. floor (!screen_height /. 28.))
+         (13. *. floor (!screen_width /. 40.))
+         (2. *. floor (!screen_height /. 25.))
+    :: create_rectangle
+         (26. *. floor (!screen_width /. 40.))
+         (0. *. floor (!screen_height /. 28.))
+         (2. *. floor (!screen_width /. 36.))
+         (15. *. floor (!screen_height /. 28.))
     :: !path_rectangles;
 
   ()
